122367a9d36a9abdb4f7c86311c8841f79ebcc21
nerner3 <neriya.cohen1@huji.mail.ac.il>
2021-10-20 21:48:00 +0300

    first function load_img
    added the normalization and np.float64

--- sol1.py
+++ sol1.py
@@ -6,6 +6,7 @@ import imageio as imao
 from skimage import color
 
 GRAY_SCALE = 1
+COLOR_RANGE = 255
 
 x = np.hstack([np.repeat(np.arange(0,50,2),10)[None,:], np.array([255]*6)[None,:]])
 grad = np.tile(x,(256,1))
@@ -15,4 +16,9 @@ def load_img(path, representation=2):
     img = imao.imread(path)
     if representation == GRAY_SCALE:
         img = color.rgb2gray(img)
+    else:
+        img = img/COLOR_RANGE
     return img
+
+img_url = 'jerusalem.jpg'
+load_img(img_url)
