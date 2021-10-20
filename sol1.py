cdfe4da376023f2beaf0f70fc0e5cbb790b619c4
nerner3 <neriya.cohen1@huji.mail.ac.il>
2021-10-20 22:30:02 +0300

    added second function

--- sol1.py
+++ sol1.py
@@ -11,8 +11,8 @@ COLOR_RANGE = 255
 x = np.hstack([np.repeat(np.arange(0,50,2),10)[None,:], np.array([255]*6)[None,:]])
 grad = np.tile(x,(256,1))
 
-
-def load_img(path, representation=2):
+"""load color image and convert to grayScale if representaion is 1 """
+def read_image(path, representation=2):
     img = imao.imread(path)
     if representation == GRAY_SCALE:
         img = color.rgb2gray(img)
@@ -20,5 +20,16 @@ def load_img(path, representation=2):
         img = img/COLOR_RANGE
     return img
 
+
+def imdisplay(path, rerepresentation=2):
+    img = read_image(path, rerepresentation)
+    # img = -1*(1 - img)  # convert the image
+    if rerepresentation == GRAY_SCALE:
+        plt.imshow(img, 'Greys')
+    else:
+        plt.imshow(img)
+    plt.show()
+
 img_url = 'jerusalem.jpg'
-load_img(img_url)
+imdisplay(img_url, 1)
+
